1369c8282badf60bb75471db137e844aacb3f1c2
Owi Wei How <owiweihow@gmail.com>
2022-03-21 13:43:37 +0800

    created func to send tele

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # notification-function
 This repo provides the source code for functions to send notification via OCI Notification and Telegram using OCI Functions.
+
+## Invoke 
+1. Invoke function via fn cli
+> echo -n '{"api_key":"<Bot_Token>", "chat_id":"<Group_Chat_ID>", "text": "<Your_Message>"}' | fn invoke send-telegram send-telegram
--- src/send-telegram/func.py
+++ src/send-telegram/func.py
@@ -16,14 +16,15 @@ def handler(ctx, data: io.BytesIO = None):
 
         tele_bot = telegram.Bot(token=apiKEY)
         bot_sendMessage = tele_bot.send_message(chat_id=chatID, text=textMessage)
-        print(str(bot_sendMessage))
         logging.getLogger().info("Message sent successfully")
+        logging.getLogger().info(bot_sendMessage)
+        send_status = 'succeed'
 
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing json payload: ' + str(ex))
+        send_status = 'failed'
 
     return response.Response(
-        ctx, response_data=json.dumps(
-            {"result": "sent successfully"}),
+        ctx, response_data=json.dumps({"status": format(send_status), "message_id" : format(bot_sendMessage.message_id)}),
         headers={"Content-Type": "application/json"}
     )
